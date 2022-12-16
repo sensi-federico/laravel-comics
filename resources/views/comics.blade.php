997c20a203f31491869df194b0f2acaeb55d46ca
sensiz <dev.federicosensi@gmail.com>
2022-12-16 17:27:26 +0100

    card generate dinamicamente

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -8,4 +8,19 @@
     </div>
 </section>
 
+<section class="comics bg-dark">
+    <div class="container">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4">
+            @foreach($thumbs as $thumb)
+            <div class="col">
+                <div class="my-card text-center text-white text-uppercase border-0">
+                    <img class="card-img-top" src="{{$thumb['thumb']}}" alt="">
+                    <p class="m-0 pt-4">{{$thumb['title']}}</p>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</section>
+
 @endsection
